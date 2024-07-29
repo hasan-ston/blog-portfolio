59cc2f6f48c23b89cd1f7f957e26c5ca9cc4c8e0
hasan-ston <mhd.hasan236@gmail.com>
2024-07-29 20:28:36 +0500

    Update wsgi.py

--- rp-portfolio/personal_portfolio/wsgi.py
+++ rp-portfolio/personal_portfolio/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
 
 application = get_wsgi_application()
 app = application
