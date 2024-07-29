66b9ede1102231afd54ff402b6b093f2471344a1
hasan-ston <mhd.hasan236@gmail.com>
2024-07-29 20:13:06 +0500

    Update settings.py

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -56,14 +56,8 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'URL': postgresql://postgres:ykLMOVCtwPJrxNDPaMCgeAJdWgegqAib@monorail.proxy.rlwy.net:38931/railway,
-        'DATABASE': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'ykLMOVCtwPJrxNDPaMCgeAJdWgegqAib',
-        'HOST': 'postgres.railway.internal',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
