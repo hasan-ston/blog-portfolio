8e3c0c97dc06ee375755f4f2275621ed0e39341d
hasan-ston <mhd.hasan236@gmail.com>
2025-10-14 04:39:46 -0400

    Update settings.py

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -53,11 +53,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 AUTH_PASSWORD_VALIDATORS = [
     {
