4f03406aa1b8c8311265219d1a6fd5b387677fc0
hasan-ston <mhd.hasan236@gmail.com>
2025-10-14 04:03:27 -0400

    Update settings.py

--- rp-portfolio/personal_portfolio/settings.py
+++ rp-portfolio/personal_portfolio/settings.py
@@ -7,7 +7,8 @@ SECRET_KEY = 'django-insecure-iyk-p_a(%bf6cvdc+v6-7+uf*mzp7t8+0(!cj10+0%i$x9g5h6
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', 'portfolio-0743.onrender.com'
+]
 
 
 INSTALLED_APPS = [
